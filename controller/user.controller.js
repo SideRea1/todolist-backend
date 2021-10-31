46fb6d43e6db45903e5c85623950f0cbd4ba24fc
SideRea1 <zvezdilin.sasha2017@yandex.ru>
2021-10-31 15:24:33 +0300

    changes

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,21 +2,21 @@ const db = require('../db')
 
 class UserController {
     async createUser(req, res) {
-        const {task, isDone} = req.body
-        const newTask = await db.query('INSERT INTO todo_list (task, isDone) values ($1, $2) RETURNING *', [task, isDone])
+        const {task} = req.body
+        const newTask = await db.query('INSERT INTO todo (task) values ($1) RETURNING *', [task])
         console.log(req.body)
 
         res.json(newTask.rows[0])
     }
 
     async getUsers(req, res) {
-        const users = await db.query('SELECT * FROM todo_list')
+        const users = await db.query('SELECT * FROM todo')
         res.json(users.rows)
     }
 
     async deleteUser(req, res) {
         const id = req.params.id
-        const user = await db.query('DELETE FROM todo_list where id = $1', [id])
+        const user = await db.query('DELETE FROM todo where id = $1', [id])
         res.json(user.rows[0])
 
     }
